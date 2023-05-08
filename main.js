83a3fdd766c61cb27d84674ca8a73b8e2620aaa1
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-05-09 02:27:36 +1000

    parsed data from a weather api

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import './style.css';
+import { getWeather } from './weather';
+
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
+  data => {
+    console.log(data);
+  }
+);
+// Intl.DateTimeFormat().resolvedOptions().timeZone gets back whatever timezone we are in
